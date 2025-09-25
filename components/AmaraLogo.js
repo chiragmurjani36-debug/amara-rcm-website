305db5d4f40e4d7fd219373b102f5ac7117c84c8
chiragmurjani36-debug <chiragmurjani36@gmail.com>
2025-09-25 17:12:31 -0400

    Update AmaraLogo.js

--- components/AmaraLogo.js
+++ components/AmaraLogo.js
@@ -1,10 +1,15 @@
 // components/AmaraLogo.js
+import Image from "next/image";
+
 export default function AmaraLogo({ className = "h-16 w-auto" }) {
   return (
-    <img
-      src="/logo.png"
+    <Image
+      src="/logo.png"   // path inside /public
       alt="Amara RCM Services Logo"
       className={className}
+      width={354}       // adjust size
+      height={193}
+      priority          // ensures logo loads fast
     />
   );
 }
