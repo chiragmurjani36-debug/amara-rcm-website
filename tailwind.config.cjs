03440226d2c12f5f7d6dacd33d3c1250a07077db
chiragmurjani36-debug <chiragmurjani36@gmail.com>
2025-09-25 18:29:36 -0400

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {
-    colors: {
-      brand: "#10233E",
-      accent: "#1F6FEB",
-      gold: "#FFC53D",
-      teal: "#00A8A8",
-      pink: "#EA4C89"
+  theme: {
+    extend: {
+      colors: {
+        brand: "#10233E",
+        accent: "#1F6FEB",
+        gold: "#FFC53D",
+        teal: "#00A8A8",
+        pink: "#EA4C89"
+      },
+      fontFamily: {
+          fredoka: ['Fredoka', 'sans-serif'],   // 👈 add this
+      },
     }
-  } },
+  },
   plugins: [],
 };
